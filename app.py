80fe9218ae9662a97cbf9183b609f63d6d648d00
Pratik More <pratikmore124@gmail.com>
2024-04-23 01:14:30 +0530

    modify app.py file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "CI CD pipline has been established"
+    return "Miss Sudipta I LOVE YOU"
 
 if __name__=="__main__":
     app.run(debug=True)
