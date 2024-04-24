cc2495e1f1ca20977209e8f79ac7d744c83a2dc6
Pratik More <pratikmore124@gmail.com>
2024-04-25 02:00:56 +0530

    added code of logging and exception

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Miss Sudipta I LOVE YOU"
+    try:
+        raise Exception("We are tryingh to raise exceoption")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+
+        logging.info("We are testing loggin module")
+    return "CI CD pipeline setup complete"
 
 if __name__=="__main__":
     app.run(debug=True)
