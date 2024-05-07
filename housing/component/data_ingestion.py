79137ef27c8ae33245fdd9f29904a2a267b33ace
Pratik More <pratikmore124@gmail.com>
2024-05-08 02:27:30 +0530

    data ingestion.py file

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -2,7 +2,9 @@ from housing.entity.config_entity import DataIngestionConfig
 import sys,os
 from housing.exception import HousingException
 from housing.logger import logging
-
+from housing.entity.artifact_entity import DataIngestionArtifact
+from six.moves import urllib   #to download the file
+import tarfile       # extract zip file
 
 class DataIngestion:
 
@@ -14,8 +16,33 @@ class DataIngestion:
             raise HousingException(e,sys) from e
         
 
+    def download_housing_data(self) -> str:
+        try:
+            # extracting remote url to download dataset
+            download_url = self.data_ingestion_config.dataset_download_url
+
+            # folder location to downlaod file
+            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
+
+            housing_file_name = os.path.basename(download_url)
+
+            tgz_file_path = os.path.join(tgz_download_dir,housing_file_name)
+            logging.info(f"Downloading file from :{download_url} into :{tgz_file_path}")
+            urllib.request.urlretrieve(download_url,tgz_file_path)
+            logging.info(f"File:{tgz_file_path} has been downlaod successful")
+            return tgz_file_path
+
+        except Exception as e:
+            raise HousingException(e,sys)
+
+    def extract_tgz_file(self):
+        pass
+
+    def split_data_as_train_test(self):
+        pass
+
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
             pass
         except Exception as e:
-            raise HousingException(e,sys) from e1
+            raise HousingException(e,sys) from e
